b223d89d91ff08b05d4a309ec61a668e02b6de23
GhostKato <osadchenko.maksym@gmail.com>
2024-12-21 02:04:10 +0200

    fixed

--- src/components/Social/Social.jsx
+++ src/components/Social/Social.jsx
@@ -7,19 +7,25 @@ const Social = ({styleIcon}) => {
     return (
         <ul className={s.list}>
             <li className={s.item}>
-                <a href="#">
+                <a href="https://www.facebook.com"
+                target="_blank"
+                rel="noopener noreferrer nofollow">
                     <Facebook styleIcon={styleIcon} />
                 </a>
             </li>
 
             <li className={s.item}>
-                <a href="#">
+                <a href="https://www.instagram.com/goitclub"
+                target="_blank"
+                rel="noopener noreferrer nofollow">
                     <Instagram styleIcon={styleIcon}/>
                 </a>
             </li>
 
             <li className={s.item}>
-                <a href="#">
+                <a href="https://www.youtube.com/@GoIT"
+                target="_blank"
+                rel="noopener noreferrer nofollow">
                     <Youtube styleIcon={styleIcon}/>
                 </a>
             </li>
